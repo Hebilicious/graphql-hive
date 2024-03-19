b87f736b780554426f934785a0cf2e3ad1b98081
TuvalSimha <tuval.simha@gmail.com>
2024-03-19 15:25:17 +0200

    Docs website: Remove patches + update docs

--- packages/web/docs/theme.config.tsx
+++ packages/web/docs/theme.config.tsx
@@ -1,4 +1,4 @@
-/* eslint sort-keys: error */
+import React from 'react';
 import { useRouter } from 'next/router';
 import { defineConfig, FooterExtended, Giscus, useConfig, useTheme } from '@theguild/components';
 import { ProductUpdateBlogPostHeader } from './src/components/product-update-blog-post-header';
