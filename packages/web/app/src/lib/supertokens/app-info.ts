cb38aaa1877bffdbed6d2fbbbee4ec3c6b001404
Kamil Kisiela <kamil.kisiela@gmail.com>
2024-03-20 15:06:14 +0100

    asd

--- packages/web/app/src/lib/supertokens/app-info.ts
+++ packages/web/app/src/lib/supertokens/app-info.ts
@@ -1,12 +1,12 @@
 import { env } from '@/env/frontend';
 
 export const appInfo = () => {
-  const { appBaseUrl, graphqlPublicDomain } = env;
+  const { appBaseUrl, graphqlPublicOrigin } = env;
 
   return {
     // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
     appName: 'GraphQL Hive',
-    apiDomain: graphqlPublicDomain,
+    apiDomain: graphqlPublicOrigin,
     websiteDomain: appBaseUrl,
     apiBasePath: '/auth',
     websiteBasePath: '/auth',
